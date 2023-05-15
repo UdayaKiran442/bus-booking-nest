809b41ada10b120b7a9659fadca2d268e00f27e5
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-05-15 13:34:07 +0530

    connected to database and stored secret variables in .env

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'nest-bus-booking',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
